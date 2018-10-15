8ab5c0224560266e619563f279f899ce157ef858
Rikki <rakeshkamboj91@gmail.com>
2018-10-16 02:50:37 +0530

    more changes

--- src/com/Employee.java
+++ src/com/Employee.java
@@ -1,9 +1,49 @@
 package com;
 
-public class Employee implements java.io.Serializable {  
-	private int id;  
-	private String firstName,lastName;  
-	  
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.Columns;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+@Entity(name="EMPLOYEE_DETAILS")
+public class Employee implements java.io.Serializable {
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private int id; 
+	
+	@Column(name="First_Name")
+	private String firstName; 
+	
+	@Column(name="Last_Name") 
+	private String lastName;  
+	
+	@ElementCollection
+	@JoinTable(name="USER_ADDRESS",
+				joinColumns=@JoinColumn(name="USER_ID")
+			)
+	
+	@GenericGenerator(name = "hilo-gen", strategy = "hilo")
+	@CollectionId(columns ={ @Column(name="address_id")}, generator = "hilo-gen", type = @Type(type="long") )
+	private Collection<Address> address=new ArrayList<Address>();
+
+	public Collection<Address> getAddress() {
+		return address;
+	}
+	public void setAddress(Collection<Address> address) {
+		this.address = address;
+	}
 	public int getId() {  
 	    return id;  
 	}  
---
+++ src/com/TestFetch.java
@@ -0,0 +1,21 @@
+package com;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class TestFetch {
+	
+	public static void main(String obj[]){
+		
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = s.beginTransaction(); 
+        Employee employee=null;
+        
+        employee = (Employee) s.get(Employee.class, 1);
+        System.out.println(employee.getFirstName()+" "+employee.getLastName());
+        
+       // s.close();
+        t.commit();
+         
+	}
+}
